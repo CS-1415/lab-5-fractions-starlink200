0769af0d48231570408059c8f4f6610c46e094b7
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2025-02-19 23:13:50 -0700

    created library and test projects

--- obj/Debug/net8.0/lab-5-fractions-starlink200.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-5-fractions-starlink200.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-5-fractions-starlink200")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f1c37815e0915d52bf9f08e9c2570014fb93cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80e35f80d325a2c4ea2b16a317298fecfe29b1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-5-fractions-starlink200")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-5-fractions-starlink200")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
